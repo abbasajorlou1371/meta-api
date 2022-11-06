7c222777ae5b85417adcccc293e88d257d79b632
Laravel Developer <abbas.ajorlou1371@gmail.com>
2022-11-06 12:44:53 +0330

    email and payment verification redirect url fixed

--- app/Http/Controllers/Auth/EmailVerificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -16,8 +16,9 @@ class EmailVerificationController extends Controller
         if ($user->hasVerifiedEmail()) {
             return response()->json(['error' => 'آدرس ایمیل قبلا تایید شده است']);
         }
-        
+
         $user->markEmailAsVerified();
-        return redirect('https://rgb.irpsc.com');
+
+        return redirect()->to('https://rgb.irpsc.com/email/verified=true');
     }
 }
