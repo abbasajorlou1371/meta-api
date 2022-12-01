a6bb24be71c61148df97b9a1fc265d91d0bb7ef8
Laravel <abbas.ajorlou1371@gmail.com>
2022-12-01 10:41:09 +0330

    Small bug fix

--- app/Http/Middleware/AccountSecurity.php
+++ app/Http/Middleware/AccountSecurity.php
@@ -20,7 +20,7 @@ class AccountSecurity
         $accountSecurity = $request->user()->accountSecurity;
         if (is_null($accountSecurity) || time() > $accountSecurity?->until) {
             return $request->expectsJson() ?
-                abort(403, 'جهت ادامه شماره تلفن خود را تایید کنید!')
+                abort(403, 'جهت ادامه امنیت حساب کاربری خود را غیر فعال کنید!')
                 : RouteServiceProvider::HOME;
         }
         $accountSecurity->update(['last_activity' => time()]);
