045ef0dc63bec96bfad9023c96a10ce76fd6844d
admin <admin@info.com>
2023-02-02 12:33:26 +0330

    top player list bug fixed

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,21 +5,22 @@ namespace App\Repositories;
 use App\Models\User;
 use Illuminate\Support\LazyCollection;
 
-class UserRepository extends Repository {
-    public function getTopPlayers(): LazyCollection
+class UserRepository extends Repository
+{
+    public function getTopPlayers()
     {
         return User::orderBy('score', 'DESC')
-        ->take(10)
-        ->lazyById()
-        ->map(function($user) {
-            return [
-                'id'             => $user->id,
-                'code'           => $user->code,
-                'score'          => $user->score,
-                'profile_photos' => [$user->profilePhotos->last()],
-                'level'          => $user->level,
-                'online'         => $user->last_seen->diffInMinutes(now()) < 2,
-            ];
-        });
+            ->take(10)
+            ->get()
+            ->map(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'code' => $user->code,
+                    'score' => $user->score,
+                    'profile_photos' => [$user->profilePhotos->last()],
+                    'level' => $user->level,
+                    'online' => $user->last_seen->diffInMinutes(now()) < 2,
+                ];
+            });
     }
 }
