da4b5073ad2cce2d1ab69994bb8ccfc09371c9c7
abbas <abbas.ajorlou1371@gmail.com>
2023-02-14 12:52:35 +0330

    FeatureResource small bug fix

--- app/Http/Resources/FeatureResource.php
+++ app/Http/Resources/FeatureResource.php
@@ -33,7 +33,7 @@ class FeatureResource extends JsonResource
                     'rgb' => $this->properties->rgb,
                     'price_psc' => $this->properties->price_psc,
                     'price_irr' => $this->properties->price_irr,
-                    $this->mergeWhen($this->latestTraded, [
+                    $this->mergeWhen(!is_null($this->latestTraded), [
                         'date' => Jalalian::forge($this->latestTraded->created_at)->format('Y/m/d'),
                     ])
                 ],
