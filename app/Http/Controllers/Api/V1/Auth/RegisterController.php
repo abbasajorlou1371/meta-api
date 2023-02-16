e827e428611f8404b5da23533344c7e3f087510c
abbas <abbas.ajorlou1371@gmail.com>
2023-02-16 14:21:52 +0330

    RegisterController updated

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -3,29 +3,64 @@
 namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RegisterRequest;
 use App\Models\Referal;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
-    /**
-     * @param RegisterRequest $request
-     * @param $referral
-     * @return JsonResponse
-     * @throws ValidationException
-     */
-    public function register(RegisterRequest $request)
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+    use RegistersUsers;
 
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make(
+            $data,
+            [
+                'name' => 'required|string|min:2',
+                'email' => 'required|email|unique:users,email',
+                'password' => [
+                    'required',
+                    Password::defaults(),
+                ],
+                'referral' => 'nullable|exists:users,code'
+            ],
+            [
+                'name' => [
+                    'required' => 'نام خود را وارد کنید',
+                    'string' => 'نام صحیح نمی باشد'
+                ],
+                'email' => [
+                    'required' => 'ایمیل را وارد کنید',
+                    'email' => 'آدرس ایمیل صحیح نیست',
+                    'unique' => 'آدرس ایمیل قبلا استفاده شده است'
+                ],
+                'password.required' => 'رمز عبور را وارد کنید',
+                'referral.exists' => 'لینک رفرال صحیح نیست.'
+            ]
+        );
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('web');
+    }
+
+    protected function registered(Request $request, $user)
+    {
         if ($request->has('referral')) {
             $reference_user = User::firstWhere('code', $request->referral);
             Referal::create([
@@ -33,11 +68,7 @@ class RegisterController extends Controller
                 'referer_id' => $user->id,
             ]);
         }
-
         $user->registered();
-
-        event(new Registered($user));
-
-        return response()->noContent(200);
+        return response()->noContent();
     }
 }
