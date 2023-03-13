21178112b65f79381408225f27e6929a73f925f1
abbas <abbas.ajorlou1371@gmail.com>
2023-03-13 10:15:01 +0330

    Change password small bug fix

--- app/Http/Controllers/Api/V1/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Api/V1/Auth/ChangePasswordController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ChangePasswordRequest;
-use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
     public function __invoke(ChangePasswordRequest $request)
     {
-        $request->user()->forceFill([
-            'password' => Hash::make($request->password)
-        ]);
+        $request->user()->update(['password' => bcrypt($request->password)]);
         return response()->noContent(200);
     }
 }
