06f05156cc89ed0d224e4c983f4bd14279f3888d
abbas <abbas.ajorlou1371@gmail.com>
2023-04-30 10:31:33 +0330

    Profile photos small bug fix

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,12 +14,5 @@ class Image extends Model
     public function imageable()
     {
         return $this->morphTo();
-    }
-
-    public function url():Attribute
-    {
-        return Attribute::make(
-            get: fn($value) => config('rgb.admin_panel_url') . $value
-        );
     }
 }
