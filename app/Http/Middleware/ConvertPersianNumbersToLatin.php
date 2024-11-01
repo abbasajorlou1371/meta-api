5cc7b9e15199a8b6b28766672e7e5c2e51dcbb3d
abbass <abbas.ajorlou1371@gmail.com>
2024-11-02 01:23:54 +0330

    Small bug fix made

--- app/Http/Middleware/ConvertPersianNumbersToLatin.php
+++ app/Http/Middleware/ConvertPersianNumbersToLatin.php
@@ -4,61 +4,34 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ConvertPersianNumbersToLatin
 {
     /**
-     * Convert Persian numbers in the request to Latin numbers.
+     * Handle an incoming request.
      *
-     * @param Request $request The request object.
-     * @param Closure $next The next middleware in the pipeline.
-     * @return mixed The response from the next middleware.
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        $input = $request->all();
-
-        if(count($input) == 0) {
-            return $next($request);
-        }
-
-        $converted = $this->convertPersianNumbersToLatin($input);
-        $request->merge($converted);
+        $request->merge(array_map(function ($value) {
+            return is_string($value) ? $this->convertPersianNumbersToLatin($value) : $value;
+        }, $request->all()));
 
         return $next($request);
     }
 
     /**
-     * Convert Persian numbers in the given array to Latin numbers.
+     * Convert Persian numbers to Latin
      *
-     * @param array $array The array to convert.
-     * @return array The converted array.
+     * @param string $string
+     * @return string
      */
-    private function convertPersianNumbersToLatin(array $array): array
-    {
-        $converted = [];
-
-        foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                $converted[$key] = $this->convertPersianNumbersToLatin($value);
-            } else {
-                $converted[$key] = $this->convertPersianNumbersToLatinInString($value);
-            }
-        }
-
-        return $converted;
-    }
-
-    /**
-     * Convert Persian numbers in the given string to Latin numbers.
-     *
-     * @param string $string The string to convert.
-     * @return string The converted string.
-     */
-    private function convertPersianNumbersToLatinInString(string $string): string
+    private function convertPersianNumbersToLatin(string $string): string
     {
         $persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-        $latinNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $latinNumbers = range(0, 9);
 
         return str_replace($persianNumbers, $latinNumbers, $string);
     }
