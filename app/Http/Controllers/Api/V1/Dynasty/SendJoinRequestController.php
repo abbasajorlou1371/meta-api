bae56351eb4c1241561c4e423e26726883e7ec1b
abbass <abbas.ajorlou1371@gmail.com>
2025-06-12 09:25:56 +0330

    Update JoinRequestController to load latestProfilePhoto through toUser relationship for optimized data retrieval

--- app/Http/Controllers/Api/V1/Dynasty/SendJoinRequestController.php
+++ app/Http/Controllers/Api/V1/Dynasty/SendJoinRequestController.php
@@ -40,7 +40,7 @@ class SendJoinRequestController extends Controller
     public function show(JoinRequest $joinRequest)
     {
         $this->authorize('view', $joinRequest);
-        $joinRequest->load('toUser', 'requestPrize', 'latestProfilePhoto');
+        $joinRequest->load('toUser.latestProfilePhoto', 'requestPrize');
         return new SentRequestsResource($joinRequest);
     }
 
