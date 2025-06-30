f0e720c0ea031f2c97d486e66a1c29c2393240f2
abbass <abbas.ajorlou1371@gmail.com>
2025-06-30 17:22:26 +0330

    small bug fix

--- database/seeders/FixFeatureRgbSeeder.php
+++ database/seeders/FixFeatureRgbSeeder.php
@@ -15,20 +15,25 @@ class FixFeatureRgbSeeder extends Seeder
      */
     public function run(): void
     {
+        // Get IDs of features processed in the first group
+        $processedFeatureIds = [];
+
         Feature::with('properties')
             ->whereHas('sellRequests', function ($query) {
                 $query->where('status', 0);
             })
-            ->chunkById(100, function ($features) {
+            ->chunkById(100, function ($features) use (&$processedFeatureIds) {
                 foreach ($features as $feature) {
                     $feature->properties->update([
                         'rgb' => $feature->changeStatusToSoldAndPriced(),
                     ]);
+                    $processedFeatureIds[] = $feature->id;
                 }
             });
 
         Feature::with('properties')
             ->where('owner_id', '!=', 1)
+            ->whereNotIn('id', $processedFeatureIds)  // Exclude already processed features
             ->chunkById(100, function ($features) {
                 foreach ($features as $feature) {
                     $feature->properties->update([
